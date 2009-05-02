be3f6ca0bb2bba2461b976bbf9374aa3be6fb56d
gattuso (Jean-Pierre Gattuso) <gattuso@obs-vlfr.fr>
2009-05-02 09:06:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seacarb@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/seacarb/R/amp.R
+++ pkg/seacarb/R/amp.R
@@ -1,4 +1,4 @@
-# Copyright (C) 2003 Jean-Pierre Gattuso and Aurelien Proye
+# Copyright (C) 2009 Jean-Pierre Gattuso
 #
 # This file is part of seacarb.
 #
@@ -9,14 +9,9 @@
 # You should have received a copy of the GNU General Public License along with seacarb; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-"phinsi" <- function(PH=8.2, ALK=2.4e-3, Tinsi=20, Tlab=25, S=35, Pt = 0, Sit = 0, k1k2='l', kf='pf', pHscale="T") {
-	# according to Hunter (1998)
-	#si TA=0 alors calculer TA: TA=660+47.6 * S
-	dat1 <- carb(flag = 8, var1 = PH, var2 = ALK, S = S, T = Tlab, P = 0, Pt = Pt, Sit = Sit, k1k2=k1k2, kf=kf, pHscale=pHscale)
-	DIC <- dat1$DIC
-	dat2 <- carb(flag = 15, var1 = ALK, var2 = DIC, S = S, T = Tinsi, P = 0, Pt = Pt, Sit = Sit, k1k2=k1k2, kf=kf, pHscale=pHscale)
-	ph_insi <- dat2$pH
-	#utiliser DIC et TA pour calculer pH in situ (flag=15)
-	#attr(bor,"unit") <- "mol/kg"
-	return(ph_insi)
+"amp" <- 
+function(S=35,T=25){
+	amp = (111.35+5.44875*S)*1/(T+273.15)+41.6775-0.015683*S-6.20815*log(T+273.15)-log10(1-0.00106*S)
+	attr(amp,"unit") <- "mol/kg"
+	return(amp)
 }
